3b6451f637f73174d959393e281792853bc930d9
Vladimir Zhirov <vladimirzhirov@Vladimirs-MacBook-Air.local>
2017-03-30 21:28:14 +0300

    Initial Commit

---
+++ fromVCtoVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fromVCtoVC
+//
+//  Created by Vladimir Zhirov on 3/30/17.
+//  Copyright © 2017 Vladimir Zhirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
